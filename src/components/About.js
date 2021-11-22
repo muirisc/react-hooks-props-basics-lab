fce7fbef9567c5a63c325159ecf0430a4400cdba
muirisc <91299876+muirisc@users.noreply.github.com>
2021-11-21 21:56:36 -0500

    commit tree 2319bbb3287e2c91527591bb3903e475cc23233e

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,32 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  if(!props.bio){
+    return (
+      <div id="about">
+      <h2>About Me</h2>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
+      </div>
+    )
+  } else {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
-}
+      <Links github={props.github} linkedin={props.linkedin} />
+     </div>
+  )}
+};
+
 
 export default About;
+
+
+// Update the About component so that:
+
+// If a prop of bio is passed to this component, it is displayed inside of a <p> tag
+// If a prop of bio is not passed down, or if bio is an empty string, don't display the <p> tag
+// Hint: you'll need to do some conditional rendering (Links to an external site.) here to determine whether or not to display the <p> tag
--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,34 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+// import Links from "./Links";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
+
+
 console.log(user);
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar/>
+      <Home username={user.name} city={user.city} color={user.color}/>
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin} />
+      {/* <Links /> */}
     </div>
   );
 }
 
 export default App;
+
+
+// In the App component, pass down data from the user object as props to the Home component
+//  so that it renders correctly. There is some starter code for you in the Home component that should give you a clue
+//   as to what props this component takes in.
+
+// App should also pass down a prop of bio to the About component.
+
+// Finally, App should also pass down the github and linkedin links to the About component,
+//  so that About can pass those props down to the Links component you will create.
+
